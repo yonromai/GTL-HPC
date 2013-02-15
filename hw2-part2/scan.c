86cddc3bab18b2079d86a3ee46c4cc1ca3df5597
YBadiss <yacine.badiss@gmail.com>
2013-02-15 17:10:06 +0100

    removed cilk define

--- hw2-part2/scan.c
+++ hw2-part2/scan.c
@@ -22,11 +22,11 @@ Pair op_point(const Pair z1, const Pair z2) {
 int up_sweep(Pair* c, int size) {
 	int d_max = (int) (log(size)/log(2));
 	
-	for (d = 0; d < d_max; ++d) {
+	_Cilk_for (int d = 0; d < d_max; ++d) {
 		int p = 1 << d; // pow(2,d)
 		int i = 0;
 		
-	  for (int i = p-1; i < (size-p); i += p*2) {
+	  _Cilk_for (int i = p-1; i < (size-p); i += p*2) {
 			if (gb_call_op_point) {
 				c[i+p] = op_point(c[i],c[i+p]);
 			} else {
@@ -48,7 +48,7 @@ int up_sweep(Pair* c, int size) {
 int down_sweep(Pair* c, int size) {
 	int max_d = log(size)/log(2) - 1;
 	
-  for (int d = max_d; d > 0; --d) {
+  _Cilk_for (int d = max_d; d > 0; --d) {
 		int p = 1 << (d-1);
 		for (int i = (1 << d)-1; i < (size-p); i += p*2) {
 			if (gb_call_op_point) {
@@ -81,7 +81,7 @@ int scan(double* out, const double* a, const double* b, const unsigned int size,
 	
 	gb_call_op_point = (cross != NULL);
 	
-	for (int i = 0; i < size; ++i) {
+	_Cilk_for (int i = 0; i < size; ++i) {
 		c[i].first = a[i];
 		c[i].second = gb_call_op_point?b[i]:0;
 	}
@@ -93,7 +93,7 @@ int scan(double* out, const double* a, const double* b, const unsigned int size,
 	up_sweep(c, size);
 	down_sweep(c, size);
 	
-	for (int i = 0; i < size; ++i) {
+	_Cilk_for (int i = 0; i < size; ++i) {
 		out[i] = gb_call_op_point ? c[i].second : c[i].first;
 	}
 	
