9ec0f307dba0934161253d7f133eabbd6bb43f9a
Romain Yon <romain.yon@gmail.com>
2013-02-17 22:34:10 +0100

    yeah

--- hw2-part2/scan.h
+++ hw2-part2/scan.h
@@ -2,10 +2,10 @@
 #define H_SCAN
 
 #include <stdlib.h>
-// #include <cilk/cilk.h>
+#include <cilk/cilk.h>
 #include <math.h>
 
-//#define cilk_for for
+//#define _Cilk_for for
 
 typedef double (*bin_operator) (const double a, const double b);
 
